05b1d764ef0013f5a23beac0ef678a8f54cab15b
samich <itssami@gmail.com>
2023-05-29 17:19:08 -0400

    Add project files.

---
+++ MVCApp1Employees/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCApp1Employees.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
